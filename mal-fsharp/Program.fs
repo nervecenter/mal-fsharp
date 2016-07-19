09e701b674c869596be7be125de4d0bcb19f63b3
unknown <unknown@unknown.com>
2016-07-19 04:08:40 -0400

    Added gitignore. Implemented skeleton read, eval, and print.

--- mal-fsharp/Program.fs
+++ mal-fsharp/Program.fs
@@ -1,7 +1,27 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+
+let read (input : string) =
+    input
+
+let eval (readValue : string) =
+    readValue
+
+let print (evalValue : string) =
+    match evalValue with
+    | "exit" -> printfn "Goodbye."
+                evalValue
+    | _ -> printfn "%s" evalValue
+           evalValue
+
+let rep (input: string) =
+    read input |> eval |> print
+
+let rec loop () =
+    printf "user> "
+    match rep (Console.ReadLine()) with
+    | "exit" -> 0
+    | _ -> loop ()
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv : int = 
+    loop ()
